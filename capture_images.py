6deeb2438fb3294535c69a9b3ab017f517cfde82
rsmillersf <46606952+rsmillersf@users.noreply.github.com>
2023-10-25 22:37:57 +0000

    updates

--- capture_images.py
+++ capture_images.py
@@ -10,12 +10,12 @@ camera = picamera2.Picamera2()
 
 for i in range(num_images):
     image_name = f"image_{i+1}.jpg"
-    capture_config = picam2.create_still_configuration()
-    picam2.start(show_preview=True)
+    capture_config = camera.create_still_configuration()
+    camera.start(show_preview=True)
     
     time.sleep(5)
 
-    picam2.switch_mode_and_capture_file(capture_config, "image.jpg")
+    camera.switch_mode_and_capture_file(capture_config, "image.jpg")
 
     print(f"Captured {image_name}")
     

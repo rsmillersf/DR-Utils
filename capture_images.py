7ddcb0f34d7d078b2fc832bc7741086ae39a07ee
rsmillersf <46606952+rsmillersf@users.noreply.github.com>
2023-10-25 22:55:51 +0000

    Simple

--- capture_images.py
+++ capture_images.py
@@ -1,22 +1,5 @@
 import picamera2
 import time
 
-# Setup
-num_images = 20
-interval_seconds = 5.5  # Account for the 0.5s freeze effect
-
-# Capture images
 camera = picamera2.Picamera2()
-
-for i in range(num_images):
-    image_name = f"image_{i+1}.jpg"
-    capture_config = camera.create_still_configuration()
-    camera.start(show_preview=True)
-    
-    time.sleep(5)
-
-    camera.switch_mode_and_capture_file(capture_config, "image.jpg")
-
-    print(f"Captured {image_name}")
-    
-print("finished")
+picam2.start_and_capture_files("test{:d}.jpg", show_preview=True, initial_delay=5, delay=5, num_files=10)

6e89ecaa948df0c4cbbc3dc44d0259fc18f1eba0
rsmillersf <46606952+rsmillersf@users.noreply.github.com>
2023-10-25 22:10:18 +0000

    First commit

--- capture_images.py
+++ capture_images.py
@@ -1,44 +1,31 @@
-import picamera
-from PIL import Image
+import picamera2 as PiCamera2
 import time
 
 # Setup
 num_images = 20
-interval_seconds = 5
-image_directory = "/path/to/save/directory/"  # Adjust to your desired path
+interval_seconds = 5.5  # Account for the 0.5s freeze effect
 
-with picamera.PiCamera() as camera:
-    # Start the camera preview
+with PiCamera2() as camera:
     camera.start_preview()
     time.sleep(2)  # Give the camera some time to warm up
     
     for i in range(num_images):
-        # Annotate the current image count on the preview
-        camera.annotate_text = f"Capturing image {i+1} of {num_images}"
-        
         image_name = f"image_{i+1}.jpg"
-        image_path = image_directory + image_name
         
-        # Capture and save the image
-        camera.capture(image_path)
+        # Capture the image and save it in the current directory
+        camera.capture(image_name)
         print(f"Captured {image_name}")
         
-        # Stop the preview
-        camera.stop_preview()
-        
-        # Show the captured image using PIL for a brief moment
-        img = Image.open(image_path)
-        img.show()
+        # Freeze effect: Show the captured image for 0.5 seconds
+        camera.show(image_name)
         time.sleep(0.5)
         
-        # Start the preview again
+        # Continue with live preview
         camera.start_preview()
         
-        # If it's not the last image, wait for the next interval minus the half-second "freeze"
         if i < num_images - 1:
-            time.sleep(interval_seconds - 0.5)
+            time.sleep(interval_seconds - 0.5)  # Minus the freeze effect duration
     
-    # Stop the camera preview
     camera.stop_preview()
 
 print("Finished capturing images.")

da89466139241c56535750ba99a34da2a08f5040
rsmillersf <46606952+rsmillersf@users.noreply.github.com>
2023-10-25 22:36:47 +0000

    updates

--- capture_images.py
+++ capture_images.py
@@ -7,25 +7,16 @@ interval_seconds = 5.5  # Account for the 0.5s freeze effect
 
 # Capture images
 camera = picamera2.Picamera2()
-camera.start()
 
 for i in range(num_images):
     image_name = f"image_{i+1}.jpg"
+    capture_config = picam2.create_still_configuration()
+    picam2.start(show_preview=True)
     
-    # Capture the image and save it in the current directory
-    camera.capture_image(image_name)
+    time.sleep(5)
+
+    picam2.switch_mode_and_capture_file(capture_config, "image.jpg")
+
     print(f"Captured {image_name}")
     
-    # Freeze effect: Show the captured image for 0.5 seconds
-    camera.show(image_name)
-    time.sleep(0.5)
-    
-    # Continue with live preview
-    camera.start_preview()
-    
-    if i < num_images - 1:
-        time.sleep(interval_seconds - 0.5)  # Minus the freeze effect duration
-
-camera.stop_preview()
-
-print("Finished capturing images.")
+print("finished")
